63f7d348128839ad1bbe5d4037d86696e8fa9a4a
Kasper Navnebeskyttet (KASP789Z) <kasp789z@zbc.dk>
2024-03-12 08:04:10 +0100

    Update

--- ScreenManager1/ScreenManager1/obj/Debug/net8.0/ScreenManager1.AssemblyInfo.cs
+++ ScreenManager1/ScreenManager1/obj/Debug/net8.0/ScreenManager1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenManager1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bae92a7ead86eeb904e29ed1b112be4405206a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+553b0f010441c103b6e431753a792e5ba0b6473d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenManager1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenManager1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
